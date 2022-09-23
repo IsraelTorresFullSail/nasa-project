42954fccc69e7f61ecf9629fb6b7c1531c66f3d2
Israel Torres <israel.a.torres01@gmail.com>
2022-09-23 15:19:28 -0400

    NASA project completed

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -7,7 +7,7 @@ const launch = {
   mission: 'Kepler Exploration X',
   rocket: 'Explorer IS1',
   launchDate: new Date('December 27, 2030'),
-  destination: 'Kepler-442 b',
+  target: 'Kepler-442 b',
   customer: ['ZTM', 'NASA'],
   upcoming: true,
   success: true,
@@ -15,9 +15,14 @@ const launch = {
 
 launches.set(launch.flightNumber, launch)
 
+function existLaunchWithId(launchId) {
+  return launches.has(launchId)
+}
+
 function getAllLaunches() {
   return Array.from(launches.values())
 }
+
 function addNewLaunch(launch) {
   latestFlightNumber++
   launches.set(
@@ -31,7 +36,16 @@ function addNewLaunch(launch) {
   )
 }
 
+function abortLaunchById(launchId) {
+  const aborted = launches.get(launchId)
+  aborted.upcoming = false
+  aborted.success = false
+  return aborted
+}
+
 module.exports = {
+  existLaunchWithId,
   getAllLaunches,
   addNewLaunch,
+  abortLaunchById,
 }
